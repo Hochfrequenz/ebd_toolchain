dd97b6afd20ba35b2bd3d004c0d51c87183f1a6c
olli <144932831+OLILHR@users.noreply.github.com>
2024-05-05 15:53:10 +0200

    isort

--- src/ebd_toolchain/main.py
+++ src/ebd_toolchain/main.py
@@ -19,7 +19,6 @@ from typing import Literal
 
 import cattrs
 import click
-
 from ebdamame import TableNotFoundError, get_all_ebd_keys, get_ebd_docx_tables
 from ebdamame.docxtableconverter import DocxTableConverter
 from rebdhuhn.graph_conversion import convert_table_to_graph
